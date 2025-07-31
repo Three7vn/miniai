6230722e6d7bd91a8843634b9890336a1e4d05bf
Three7vn <abraham@stoado.com>
2025-07-31 21:59:10 +0200

    remove grammar check

--- format.py
+++
@@ -1,9 +0,0 @@
-from openai import OpenAI
-from dotenv import load_dotenv
-
-load_dotenv()
-
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-
-response = client.chat.completions.create(
-    
